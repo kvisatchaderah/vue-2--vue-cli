d40cf0417eb1c24ce81bb1843ad0cf873f28803e
kvisatchaderah <kvisatchaderah@gmail.com>
2022-05-28 13:33:37 +0300

    .

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/"> home </router-link>
-      <router-link to="/fifteen_canvas"> пятнашки canvas </router-link>
-      <router-link to="/fifteen_node"> пятнашки node 1 </router-link>
-      <router-link to="/grafik_canvas"> grafik_canvas </router-link>
-    </nav>
+    <b-container>
+      <b-row class="my-3">
+        <b-col><router-link to="/"> home </router-link></b-col>
+        <b-col>
+          <router-link to="/fifteen_canvas"> пятнашки canvas </router-link>
+        </b-col>
+        <b-col>
+          <router-link to="/fifteen_node"> пятнашки node </router-link>
+        </b-col>
+        <b-col>
+          <router-link to="/grafik_canvas"> grafik_canvas </router-link>
+        </b-col>
+      </b-row>
+    </b-container>
 
-    <router-view />
+    <b-container>
+      <router-view />
+    </b-container>
+
+    <FifteenNode />
   </div>
 </template>
+
+<script>
+  import { BContainer, BRow, BCol } from 'bootstrap-vue'
+  import FifteenNode from '@/views/fifteen_node/fifteen_node.vue'
+
+  export default {
+    components: {
+      BContainer,
+      BRow,
+      BCol,
+      FifteenNode,
+    },
+  }
+</script>
 
 <style lang="sass">
   @import 'sass/template.sass'
 </style>
-
-<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/sass/template.sass'
 
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
