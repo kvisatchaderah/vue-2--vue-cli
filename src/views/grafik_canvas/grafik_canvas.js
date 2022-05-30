ab0daf10c8b3980825e19ac294e69dae2bfc2a6d
kvisatchaderah <kvisatchaderah@gmail.com>
2022-05-30 20:41:04 +0300

    п

--- src/views/grafik_canvas/grafik_canvas.js
+++ src/views/grafik_canvas/grafik_canvas.js
@@ -2,17 +2,27 @@ export default {
   // data
   data() {
     return {
-      grafik_settings: { x_max: 5, y_max: 5, width: 700, height: 500 },
-      context: null,
-      grafik_settings_in_node: { x_max: 5, y_max: 5, width: 700, height: 500 },
+      settings: null,
+      ctx: null,
+      settings_in_node: { x_max: 5, y_max: 5, width: 500, height: 500 },
       active_function: null,
       functions: [
         { title: 'X<sup>0.5</sup>', function: (a) => a ** 0.5 },
-        { title: 'X<sup>1</sup>', function: (a) => a },
+        { title: '-X', function: (a) => -a },
         { title: 'X<sup>2</sup>', function: (a) => a ** 2 },
         { title: 'X<sup>3</sup>', function: (a) => a ** 3 },
       ],
     }
+  },
+
+  // computed
+  computed: {
+    grid_width() {
+      return this.settings.width / (2 * this.settings.x_max)
+    },
+    grid_height() {
+      return this.settings.height / (2 * this.settings.y_max)
+    },
   },
 
   // created
@@ -23,32 +33,102 @@ export default {
   // mounted
   mounted() {
     this.init_canvas()
+    this.start_grafik()
   },
 
   // methods
   methods: {
+    //
     // init
+    //
     init_canvas() {
-      this.context = grids.getContext('2d')
+      this.ctx = grids.getContext('2d')
     },
-    set_settings() {
-      this.grafik_settings = { ...this.grafik_settings_in_node }
+    clear_canvas() {
+      this.ctx.clearRect(0, 0, this.settings.width, this.settings.height)
     },
-    start_grafik() {
-      this.set_settings()
-      this.paint_grids()
-    },
+
     set_function(f) {
       this.active_function = { ...f }
     },
+    set_settings() {
+      this.settings = { ...this.settings_in_node }
+    },
+
+    start_grafik() {
+      this.clear_canvas()
+      this.set_settings()
+      this.paint_grids()
+      this.paint_legend()
+    },
     new_function(f) {
       this.set_function(f)
-      this.start_grafik()
+      this.clear_canvas()
+    },
+
+    //
+    // paint helpers
+    //
+    set_stroke_style(val) {
+      this.ctx.strokeStyle = `${val}`
+    },
+    set_null_ledge(num) {
+      return num ? 6 : 0
+    },
+
+    // guide
+    paint_guide_horizontal(num) {
+      const null_ledge = this.set_null_ledge(num)
+
+      this.ctx.beginPath()
+      this.ctx.moveTo(
+        null_ledge,
+        this.settings.height / 2 + num * this.grid_height
+      )
+      this.ctx.lineTo(
+        this.settings.width - null_ledge,
+        this.settings.height / 2 + num * this.grid_height
+      )
+      this.ctx.stroke()
+      this.ctx.closePath()
+    },
+
+    paint_guide_vertical(num) {
+      const null_ledge = this.set_null_ledge(num)
+
+      this.ctx.beginPath()
+      this.ctx.moveTo(
+        this.settings.width / 2 + num * this.grid_width,
+        null_ledge
+      )
+      this.ctx.lineTo(
+        this.settings.width / 2 + num * this.grid_width,
+        this.settings.height - null_ledge
+      )
+      this.ctx.stroke()
+      this.ctx.closePath()
     },
 
     // paint
     paint_grids() {
-      // this.context.
+      this.set_stroke_style('#ffffff')
+      this.paint_guide_horizontal(0)
+      this.paint_guide_vertical(0)
+
+      this.set_stroke_style('#cecece50')
+      for (let i = 1; i < this.settings.y_max; i++) {
+        this.paint_guide_horizontal(i)
+        this.paint_guide_horizontal(-i)
+      }
+      for (let i = 1; i < this.settings.x_max; i++) {
+        this.paint_guide_vertical(i)
+        this.paint_guide_vertical(-i)
+      }
+    },
+
+    paint_legend() {
+      this.ctx.font = '12px Roboto'
+      this.ctx.fillText = '0'
     },
   },
 }
--- src/views/grafik_canvas/grafik_canvas.vue
+++ src/views/grafik_canvas/grafik_canvas.vue
@@ -1,10 +1,10 @@
 <template>
   <section id="grafik_canvas" class="grafik_canvas">
     <!-- settings -->
-    <div class="settings d-flex mb-3">
+    <div class="settings d-flex mt-5 mb-3">
       <span class="m-1"> X: </span>
       <b-form-input
-        v-model="grafik_settings_in_node.x_max"
+        v-model="settings_in_node.x_max"
         class="gc__btn btn-dark m-1"
         type="number"
         min="1"
@@ -14,7 +14,7 @@
       />
       <span class="m-1"> Y: </span>
       <b-form-input
-        v-model="grafik_settings_in_node.y_max"
+        v-model="settings_in_node.y_max"
         class="gc__btn btn-dark m-1"
         type="number"
         min="1"
@@ -25,15 +25,42 @@
     </div>
     <!-- /settings -->
 
+    <!-- settings -->
+    <div class="settings d-flex mb-3">
+      <span class="m-1"> width: </span>
+      <b-form-input
+        v-model="settings_in_node.width"
+        class="gc__btn btn-dark m-1"
+        type="number"
+        min="1"
+        max="10"
+        @keydown.stop
+        @keydown.enter="start_grafik"
+      />
+      <span class="m-1"> height: </span>
+      <b-form-input
+        v-model="settings_in_node.height"
+        class="gc__btn btn-dark m-1"
+        type="number"
+        min="1"
+        max="10"
+        @keydown.stop
+        @keydown.enter="start_grafik"
+      />
+    </div>
+    <!-- /settings -->
+
+    <hr />
+
     <!-- functions -->
-    <div class="functions d-flex mb-3">
-      <a
+    <div class="functions d-flex mt-3 mb-3">
+      <span
         v-for="(f, index) in functions"
         :key="index"
         class="m-1 mr-3 pb-3 pr-3"
         v-html="f.title"
         @click="new_function(f.function)"
-      ></a>
+      ></span>
     </div>
     <!-- /functions -->
 
@@ -41,10 +68,8 @@
     <canvas
       id="grids"
       class="grids"
-      :style="{
-        width: grafik_settings.width + 'px',
-        height: grafik_settings.height + 'px',
-      }"
+      :width="settings.width + 'px'"
+      :height="settings.height + 'px'"
     ></canvas>
     <!-- /grids -->
   </section>
